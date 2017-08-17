f9f5f78b675d2464acc7cafcb24eeb12de3c409e
Michael McMahon <michael@brightsite.com.au>
2017-08-16 20:53:16 -0400

    updating composer for plugin updates

--- web/app/themes/majac/lib/extras.php
+++ web/app/themes/majac/lib/extras.php
@@ -31,3 +31,17 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/**
+ * Customise the content of the email copy the customer receives()
+ */
+add_filter( 'pep_customer_email_content', 'customise_content', 10, 1 );
+function customise_content($cust_mail)
+{
+    $add_to_content =
+    "<tr>
+       <th style='width:25%;text-align:left'>" . __( 'Time', 'pep_text_domain' ) . " </th>
+       <td style='width:75%'>:" . date('l jS \of F Y h:i:s A') .  "</td>
+     </tr>";
+    return $add_to_content.$cust_mail;
+}
