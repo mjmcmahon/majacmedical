3f8c0d2e483c0218b4996664520739f34af17911
Michael McMahon <michael@brightsite.com.au>
2019-05-15 15:41:55 -0400

    Added favicon

--- web/app/themes/storefront/header.php
+++ web/app/themes/storefront/header.php
@@ -14,7 +14,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link rel="icon"
+      type="image/png"
+      href="/wp-content/themes/pharmacy/assets/images/favicon.png" />
 <?php wp_head(); ?>
 </head>
 
